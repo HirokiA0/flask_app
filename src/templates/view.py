50260383ce8c1ece691fcf1e087a0856352a4e12
unknown <asahi1001bd@gmail.com>
2023-03-12 15:44:57 +0900

    1st commit

---
+++ src/templates/view.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+
+from flask import Flask, render_template
+
+#appという名前でFlaskオブジェクトをインスタンス化
+app = Flask(__name__)
+
+#---View側の設定---
+#ルートディレクトリにアクセルした場合の挙動
+@app.route('/')
+def index():
+    # return 'Hello World!'
+    return render_template('index.html')
+
+#エントリーポイント
+if __name__ == '__main__':
+    app.run()
